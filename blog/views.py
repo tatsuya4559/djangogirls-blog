fa75a8aa2e650e099cce7cb29aacd1b77a3142e6
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-02-08 09:53:06 +0900

    一部クラスビューに変更

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
 
 from .forms import CommentForm, PostForm
 from .models import Comment, Post
-
-# Create your views here.
 
 
 def post_list(request):
@@ -17,34 +17,42 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, "blog/post_detail.html", {"post": post})
 
-@login_required
-def post_new(request):
-    breakpoint()
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect("post_detail", pk=post.pk)
-    else:
+
+class NewPostView(TemplateView):
+    @method_decorator(login_required)
+    def get(self, request):
         form = PostForm()
-    return render(request, "blog/post_edit.html", {"form": form})
+        return render(request, "blog/post_edit.html", {"form": form})
+
+    @method_decorator(login_required)
+    def post(self, request):
+        form = PostForm(request.POST)
+        if not form.is_valid():
+            return render(request, "blog/post_edit.html", {"form": form})
+
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect("post_detail", pk=post.pk)
 
 
-@login_required
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect("post_detail", pk=post.pk)
-    else:
+class EditPostView(TemplateView):
+    @method_decorator(login_required)
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
         form = PostForm(instance=post)
-    return render(request, "blog/post_edit.html", {"form": form})
+        return render(request, "blog/post_edit.html", {"form": form})
+
+    @method_decorator(login_required)
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        form = PostForm(request.POST, instance=post)
+        if not form.is_valid():
+            return render(request, "blog/post_edit.html", {"form": form})
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect("post_detail", pk=post.pk)
 
 
 @login_required
@@ -67,18 +75,21 @@ def post_remove(request, pk):
     return redirect("post_list")
 
 
-def add_comment_to_post(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.save()
-            return redirect("post_detail", pk=pk)
-    else:
+class AddCommentToPostView(TemplateView):
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
         form = CommentForm()
-    return render(request, "blog/add_comment_to_post.html", {"form": form})
+        return render(request, "blog/add_comment_to_post.html", {"form": form})
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        form = CommentForm(request.POST)
+        if not form.is_valid():
+            return render(request, "blog/add_comment_to_post.html", {"form": form})
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+        return redirect("post_detail", pk=pk)
 
 
 @login_required
