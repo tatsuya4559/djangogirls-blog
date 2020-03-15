731f076788479ae1c4ee20822a9795a7286aa371
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-03-15 11:13:34 +0900

    test

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,6 +3,7 @@ import blog.views as views
 
 urlpatterns = [
     path("", views.post_list, name="post_list"),
+
     path("post/<int:pk>/", views.post_detail, name="post_detail"),
     path("post/new/", views.NewPostView.as_view(), name="post_new"),
     path("post/<int:pk>/edit/", views.EditPostView.as_view(), name="post_edit"),
