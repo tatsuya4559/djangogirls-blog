daf15551c990fee1599d3d2d6e82d62e9f57777f
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-03-15 11:11:58 +0900

    com

--- src/blog/views.py
+++ src/blog/views.py
@@ -11,7 +11,6 @@ from blog.models import Comment, Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
-
     return render(request, "blog/post_list.html", {"posts": posts})
 
 
