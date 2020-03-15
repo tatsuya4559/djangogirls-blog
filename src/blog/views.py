5d6c1e73ca7bd20298cead74048acf9023238398
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-03-15 17:26:48 +0900

    test commit

--- src/blog/views.py
+++ src/blog/views.py
@@ -12,8 +12,6 @@ from blog.models import Comment, Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
     return render(request, "blog/post_list.html", {"posts": posts})
-
-
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, "blog/post_detail.html", {"post": post})
