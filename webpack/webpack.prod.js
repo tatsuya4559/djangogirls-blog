76f9560c16a4f47a4e71bfa58c2b6db3ac57e629
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-05-31 08:30:07 +0900

    build statsの出力を追加

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,14 +1,16 @@
-const path = require("path");
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
   output: {
     // prodはPJ直下のstaticに配置する
-    path: path.resolve(__dirname, "../static/dist/js"),
-    filename: "[name].js",
+    path: path.resolve(__dirname, '../static/dist/js'),
+    filename: '[name].js',
     // [name].bundle.js
     // [id].chunk.js
   },
@@ -22,7 +24,7 @@ module.exports = merge(common, {
             loader: 'css-loader',
             options: {
               modules: {
-                localIdentName: '[hash:base64]' ,
+                localIdentName: '[hash:base64]',
               },
             },
           },
@@ -30,4 +32,17 @@ module.exports = merge(common, {
       },
     ],
   },
+  plugins: [].concat(
+    process.env.BUILD_STATS
+      ? [
+          new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            reportFilename: path.join(__dirname, './stats/report.html'),
+            defaultSizes: 'gzip',
+            generateStatsFile: true,
+            statsFilename: path.join(__dirname, './stats/stats.json'),
+          }),
+        ]
+      : []
+  ),
 });
