99db3a4fdb6f153adab1658e6d805e46690aa817
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-05-30 18:28:53 +0900

    prodのソースマップを有効化

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
+  devtool: "source-map",
   output: {
     // prodはPJ直下のstaticに配置する
     path: path.resolve(__dirname, "static/dist/js"),
