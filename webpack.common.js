3079fb75f2d52bff97a1484e7796c14f801718e6
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-05-30 17:00:44 +0900

    webpack.configを環境別に分けた

--- webpack.common.js
+++ webpack.common.js
@@ -1,20 +1,7 @@
-const path = require("path");
-
-// ファイルを分けたほうがわかりやすいね
-// https://webpack.js.org/guides/production/
-
 module.exports = {
-  mode: "development",
-  devtool: "inline=source-map",
   entry: {
     postList: "./assets/blog/js/post_list.tsx",
     // あとはこのエントリポイントに追加してくだけ
-  },
-  output: {
-    // developmentなのでdjangoの各アプリの下のstaticに配置する
-    // prodはPJ直下のstaticに配置する
-    path: path.resolve(__dirname, "blog/static/dist/js"),
-    filename: "[name].js",
   },
   module: {
     rules: [
---
+++ webpack.prod.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    // prodはPJ直下のstaticに配置する
+    path: path.resolve(__dirname, "static/dist/js"),
+    filename: "[name].js",
+  },
+});
