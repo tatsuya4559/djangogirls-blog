3b1a9e573b710858b3e053cfd904ffe6d996bb3e
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-05-31 07:04:28 +0900

    コメント移動

--- webpack/entrypoints.js
+++ webpack/entrypoints.js
@@ -1,9 +1,9 @@
+/* あとはこのエントリポイントに追加してくだけ
+ * key: 'filepath'
+ *
+ * {% load static %}
+ * <script src={% static 'js/key.bundle.js' %}></script>
+ */
 module.exports = {
-    /* あとはこのエントリポイントに追加してくだけ
-     * key: 'filepath'
-     *
-     * {% load static %}
-     * <script src={% static 'js/key.bundle.js' %}></script>
-     */
     postList: './assets/blog/js/post_list.tsx',
 }
