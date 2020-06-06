cdb24e72f077cdf209e7e3c5d25489bc64e7ddb5
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-06-06 14:41:35 +0900

    ページネーション実装

--- src/blog/api.py
+++ src/blog/api.py
@@ -5,5 +5,11 @@ from blog.models import Post
 
 
 def posts(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
-    return JsonResponse({"posts": [post.as_dict() for post in posts]})
+    begin = int(request.GET.get("begin") or 0)
+    size = int(request.GET.get("size") or 10)
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
+        "published_date"
+    )
+    return JsonResponse(
+        {"posts": [post.as_dict() for post in posts[begin : begin + size]]}
+    )
