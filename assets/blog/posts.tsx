bf3ed0649c1e419de10de6029344a827ccce7203
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-06-07 13:51:05 +0900

    chakraのthemeをインポート

--- assets/blog/posts.tsx
+++ assets/blog/posts.tsx
@@ -4,10 +4,13 @@ import ErrorBoundary from './components/ErrorBoundary';
 import PostList from './containers/PostList';
 import '../../locale/i18n';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ThemeProvider } from '@chakra-ui/core';
 
 ReactDOM.render(
   <ErrorBoundary>
-    <PostList />
+    <ThemeProvider>
+      <PostList />
+    </ThemeProvider>
   </ErrorBoundary>,
   document.getElementById('posts-block'),
 );
