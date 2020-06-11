741208feffd883324cbeac29af7e2f694965740a
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-06-11 23:04:02 +0900

    tailwind拡張

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     content: ['./assets/**/*.tsx'],
   },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          100: '#c3c8f2',
+          200: '#9ca3e3',
+          300: '#757fd7',
+          400: '#4f5aca',
+          500: '#3540b0',
+          600: '#28328a',
+          700: '#1c2464',
+          800: '#0f153f',
+          900: '#04061b',
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
