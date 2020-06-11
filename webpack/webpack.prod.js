504a6b8c1981e7927aca365097e438f9ce0594af
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-06-11 20:42:28 +0900

    cssのminifyを追加

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
@@ -10,6 +12,10 @@ module.exports = merge(common, {
     // prodはPJ直下のstaticに配置する
     path: path.resolve(__dirname, '../src/static/dist'),
     filename: 'js/[name].bundle.js',
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserJSPlugin(), new OptimizeCSSAssetsPlugin()],
   },
   module: {
     rules: [
@@ -42,6 +48,6 @@ module.exports = merge(common, {
             statsFilename: path.resolve(__dirname, 'stats/stats.json'),
           }),
         ]
-      : []
+      : [],
   ),
 });
