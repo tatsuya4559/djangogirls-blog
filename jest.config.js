333a2d572898d1f71e8b333e6c9bf863cf17bfb9
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-06-14 11:48:02 +0900

    jestでもwebpackに合わせてbabelを通すようにした

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
+  preset: 'ts-jest',
   moduleFileExtensions: ['tsx', 'ts', 'js', 'json'],
   moduleNameMapper: {
     '\\.module\\.css$': 'identity-obj-proxy',
   },
+  globals: {
+    'ts-jest': {
+      babelConfig: true
+    }
+  }
 };
