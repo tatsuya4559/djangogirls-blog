202cd462a98ecb902c43dd9100d3d0624d0ffca3
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-06-21 16:49:33 +0900

    コメント追加

--- assets/blog/hooks/useClientRect.ts
+++ assets/blog/hooks/useClientRect.ts
@@ -1,5 +1,10 @@
 import { useState, useCallback } from 'react';
 
+/**
+ * 要素のサイズを計測するためのclientRectと対象要素に設定するrefを返します。
+ *
+ * @returns clientRect, ref
+ */
 export default function useClientRect() {
   const [rect, setRect] = useState<DOMRect>();
   const ref = useCallback((node: HTMLElement | null) => {
