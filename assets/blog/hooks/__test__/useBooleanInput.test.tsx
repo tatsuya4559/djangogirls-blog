160ffc3bd927d0a308fa20fb91f1ca2ab48a1669
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-06-21 16:58:59 +0900

    汎用hooks追加

---
+++ assets/blog/hooks/__test__/useBooleanInput.test.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react-hooks';
+import useBooleanInput from '../useBooleanInput';
+
+test('初期値が反映されていること', () => {
+  {
+    const { result } = renderHook(() => useBooleanInput());
+    expect(result.current[0]).toBeFalsy();
+  }
+  {
+    const { result } = renderHook(() => useBooleanInput(true));
+    expect(result.current[0]).toBeTruthy;
+  }
+  {
+    const { result } = renderHook(() => useBooleanInput(false));
+    expect(result.current[0]).toBeFalsy();
+  }
+});
+
+describe('入力イベントに反応してvalueを更新できること', () => {
+  test('checkbox', () => {
+    const { result } = renderHook(() => useBooleanInput());
+    const { getByLabelText } = render(
+      <label>
+        test
+        <input
+          type="checkbox"
+          onChange={result.current[1]}
+          checked={result.current[0]}
+        />
+      </label>,
+    );
+    expect(result.current[0]).toBeFalsy();
+
+    act(() => {
+      fireEvent.click(getByLabelText('test'));
+    });
+
+    expect(result.current[0]).toBeTruthy();
+  });
+
+  test('radio', () => {
+    const { result } = renderHook(() => useBooleanInput());
+    const { getByLabelText } = render(
+      <label>
+        test
+        <input
+          type="radio"
+          onChange={result.current[1]}
+          checked={result.current[0]}
+        />
+      </label>,
+    );
+    expect(result.current[0]).toBeFalsy();
+
+    act(() => {
+      fireEvent.click(getByLabelText('test'));
+    });
+
+    expect(result.current[0]).toBeTruthy();
+  });
+});
---
+++ assets/blog/hooks/useBooleanInput.ts
@@ -0,0 +1,17 @@
+import { useState, useCallback, FormEvent } from 'react';
+
+/**
+ * `input[type="checkbox"]`, `input[type="radio"]`要素の入力値とハンドラーを提供します。
+ *
+ * @param initialValue 初期値
+ * @returns value, handleChange
+ */
+export default function useStringInput(initialValue: boolean = false) {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = useCallback((e: FormEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.checked);
+  }, []);
+
+  return [value, handleChange] as const;
+}
