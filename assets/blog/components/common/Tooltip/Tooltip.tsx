a3e08a7fbce203912162d803e16fcfa7582f850f
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-06-24 21:22:29 +0900

    tooltipは大きさを測らなくてもCSSで可能だった

--- assets/blog/components/common/Tooltip/Tooltip.tsx
+++ assets/blog/components/common/Tooltip/Tooltip.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import cx from 'classnames';
-import useClientRect from '../../../hooks/useClientRect';
 import styles from './Tooltip.module.css';
 
 type Props = {
@@ -17,33 +16,13 @@ const Tooltip: React.FC<Props> = ({
   className,
   children,
 }) => {
-  const [rect, ref] = useClientRect();
-
-  // childrenに対して中央に表示されるように位置を計算する
-  let positionStyle: React.CSSProperties;
-  if (placement === 'top' || placement === 'bottom') {
-    const width = rect ? rect.width : 0;
-    positionStyle = {
-      left: '50%',
-      marginLeft: `-${width / 2}px`,
-    };
-  } else {
-    const height = rect ? rect.height : 0;
-    positionStyle = {
-      top: '50%',
-      marginTop: `-${height / 2}px`,
-    };
-  }
-
   return (
     <div className={styles.wrapper}>
       {children}
       <span
-        ref={ref}
         className={cx(className, styles.tooltip, styles[placement], {
           [styles.delay]: delay,
         })}
-        style={positionStyle}
       >
         {text}
       </span>
