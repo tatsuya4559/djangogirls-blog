c75f3b32cb3401e89d9afbf486db0b91b3c4c22a
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-08-17 21:02:28 +0900

    custom querysetを利用

--- src/blog/views.py
+++ src/blog/views.py
@@ -10,7 +10,7 @@ from blog.models import Comment, Post
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
+    posts = Post.objects.published().order_by("published_date")
     return render(request, "blog/post_list.html", {"posts": posts})
 
 
